884ac01cb3ad872e43acf44dc57cd324c91a390d
Roman <dubrovin.skfo@gmail.com>
2024-07-29 22:01:35 +0300

    update AdminVoteControllerTest

--- src/test/java/ru/javaops/bootjava/web/vote/AdminVoteControllerTest.java
+++ src/test/java/ru/javaops/bootjava/web/vote/AdminVoteControllerTest.java
@@ -57,10 +57,50 @@ class AdminVoteControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    void get() {
+    @WithUserDetails(value = ADMIN_MAIL)
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(AdminVoteController.REST_URL + "/" + VOTE_GUEST_ID_ON_TODAY))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(VOTE_TO_MATCHER.contentJson(vote_to_guest_on_today));
     }
 
     @Test
-    void getByUserIdAndDate() {
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.get(AdminVoteController.REST_URL + "/" + VOTE_ID_NOT_FOUND))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getByUserIdAndDate() throws Exception {
+        perform(MockMvcRequestBuilders.get(AdminVoteController.REST_URL + "/by-user-date")
+                .param("userId", String.valueOf(UserTestData.USER_ID))
+                .param("date", DATE_2024_07_25))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(VOTE_TO_MATCHER.contentJson(vote_to_user_on_2024_07_25));
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getByUserIdAndDateNull() throws Exception {
+        perform(MockMvcRequestBuilders.get(AdminVoteController.REST_URL + "/by-user-date")
+                .param("userId", String.valueOf(UserTestData.GUEST_ID)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(VOTE_TO_MATCHER.contentJson(vote_to_guest_on_today));
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getByUserIdAndDateNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.get(AdminVoteController.REST_URL + "/by-user-date")
+                .param("userId", String.valueOf(UserTestData.GUEST_ID))
+                .param("date", DATE_2024_07_25))
+                .andDo(print())
+                .andExpect(status().isNotFound());
     }
 }
