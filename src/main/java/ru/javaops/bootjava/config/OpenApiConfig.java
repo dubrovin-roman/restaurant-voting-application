23a71cdfa18564928d95e0153c3603500c30ac61
Roman Dubrovin <dubrovin.skfo@gmail.com>
2024-07-31 16:51:58 +0300

    update description and contact in OpenApiConfig

--- src/main/java/ru/javaops/bootjava/config/OpenApiConfig.java
+++ src/main/java/ru/javaops/bootjava/config/OpenApiConfig.java
@@ -22,13 +22,13 @@ import org.springframework.context.annotation.Configuration;
                 title = "REST API documentation",
                 version = "1.0",
                 description = """
-                        Приложение по <a href='https://javaops.ru/view/bootjava'>курсу BootJava</a>
-                        <p><b>Тестовые креденшелы:</b><br>
+                        Restaurant voting application
+                        <p><b>Test credits:</b><br>
                         - user@yandex.ru / password<br>
                         - admin@gmail.com / admin<br>
                         - guest@gmail.com / guest</p>
                         """,
-                contact = @Contact(url = "https://javaops.ru/#contacts", name = "Grigory Kislin", email = "admin@javaops.ru")
+                contact = @Contact(name = "Roman Dubrovin", email = "romario.panuchi@yandex.ru")
         ),
         security = @SecurityRequirement(name = "basicAuth")
 )
